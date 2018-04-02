27d544ce0a7ffac9362023029c6fb0569f214a45
Bill Tubbs <bill.tubbs@me.com>
2018-04-02 01:27:01 -0700

    Added random array generator for speed testing

--- timers.py
+++ timers.py
@@ -1,4 +1,5 @@
 import utime
+import random
 
 def timed_function(f, *args, **kwargs):
     def new_func(*args, **kwargs):
@@ -8,3 +9,6 @@ def timed_function(f, *args, **kwargs):
         print('Function Time = {:6.3f}ms'.format(delta/1000))
         return result
     return new_func
+
+def float_array_random(n, min=-1e6, max=1e6):
+    return array('f', [random()*(max - min) + min for i in range(n)])
